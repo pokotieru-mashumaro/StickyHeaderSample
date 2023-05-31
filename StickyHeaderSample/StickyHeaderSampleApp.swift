38e058b8e53008aef5a4c7022a91f0ac4e574e56
pokotieru-mashumaro <k827umeboshi@gmail.com>
2023-05-31 16:23:43 +0900

    Initial Commit

---
+++ StickyHeaderSample/StickyHeaderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderSampleApp.swift
+//  StickyHeaderSample
+//
+//  Created by 小松虎太郎 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
